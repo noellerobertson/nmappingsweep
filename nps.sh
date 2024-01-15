2c93ea997162ca4ae1523e718ae161a077093666
Noelle Robertson <117836897+noellerobertson@users.noreply.github.com>
2024-01-14 20:51:52 -0600

    Update nps.sh

--- nps.sh
+++ nps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-# nmap Ping Sweep script. Checks if nmap is installed. If it is already installed, continues with the Ping Sweep. If not, installs nmap and then proceeds with Ping Sweep.
+# nps.sh
+# nmap Ping Sweep script. Checks if nmap is installed. If it is already installed, continues with the Ping Sweep. If not, installs nmap and then proceeds with Ping Sweep. 
 
 if ! command -v nmap >/dev/null 2>&1; then
     echo "Nmap cannot be found. Installing..."
@@ -13,14 +13,14 @@ if ! command -v nmap >/dev/null 2>&1; then
         exit 1
     fi
 
-    # Check again if nmap is now installed
+    # Check again if nmap is installed.
     if [ $? -eq 0 ]; then
       echo "Nmap was successfully installed."
       exit 1
     fi
 fi
 
-# Conduct the ping sweep and save the output to a file.
+# Conduct the ping sweep on target IP addresses and save the output to a file.
 
 target_ips="172.20.240.10 172.20.240.20 172.20.242.10 172.20.242.200 172.20.241.20 172.20.241.30 172.20.241.40 172.20.242.150"
 echo "Conducting ping sweep on the following IP addresses: $target_ips"
